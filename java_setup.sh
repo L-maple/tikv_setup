b50ba4c74a341e554a55c95a891d6f09769b1872
Maple <changjieliu@ruc.edu.com>
2020-09-20 21:29:14 +0800

    update the deps

--- java_setup.sh
+++ java_setup.sh
@@ -9,14 +9,14 @@ tar -zxvf jdk-8u261-linux-x64.tar.gz -C /usr/local/java
 isJavaExist=`cat /etc/profile | grep java`
 if [[ $isJavaExist == "" ]]
 then
-    echo 'export JAVA_HOME=/usr/local/java/jdk-8u261' >> /etc/profile
+    echo 'export JAVA_HOME=/usr/local/java/jdk1.8.0_261' >> /etc/profile
     echo 'export JRE_HOME=${JAVA_HOME}/jre'           >> /etc/profile
     echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib' >> /etc/profile
     echo 'export PATH=${JAVA_HOME}/bin:$PATH'         >> /etc/profile
 fi
 source /etc/profile
 
-ln -s /usr/local/java/jdk-8u261/bin/java /usr/bin/java
+ln -s /usr/local/java/jdk1.8.0_261/bin/java /usr/bin/java
 java -version
 
 echo "java environment has set up."
