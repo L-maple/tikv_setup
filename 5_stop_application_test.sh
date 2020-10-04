c3dec41c05de7185b27df15c43252de3df66f692
Maple <changjieliu@ruc.edu.com>
2020-10-04 18:31:59 +0800

    update

--- 5_stop_application_test.sh
+++ 5_stop_application_test.sh
@@ -8,13 +8,5 @@ then
 fi
 echo "recommendation_deployment has been deleted..."
 
-# 删除kafka producer的deployment
-kafka_producer_name=`kubectl get deploy -n tidb-cluster | grep kafka-producer-deployment | awk '{print $1}'`
-if [[ $kafka_producer_name != "" ]]
-then
-    kubectl delete deploy $kafka_producer_name -n tidb-cluster
-fi
-echo "kafka-producer-deployment has been deleted..."
-
 echo "Success!"
 
