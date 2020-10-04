3293a1d95bd96155c6ab2aeffd75299b3837716f
Maple <changjieliu@ruc.edu.com>
2020-10-04 16:45:57 +0800

    update 5_stop_application_test.sh

--- 5_stop_application_test.sh
+++ 5_stop_application_test.sh
@@ -2,10 +2,16 @@
 
 # 删除示范应用部署的deployment
 recommendation_deployment_name=`kubectl get deploy -n tidb-cluster | grep recommendation_deployment | awk '{print $1}'`
-kubectl delete deploy $recommendation_deployment_name -n tidb-cluster
+if [[ $recommendation_deployment_name != "" ]]
+then
+    kubectl delete deploy $recommendation_deployment_name -n tidb-cluster
+fi
 
 # 删除kafka producer的deployment
 kafka_producer_name=`kubectl get deploy -n tidb-cluster | grep kafka-producer-deployment | awk '{print $1}'`
-kubectl delete deploy $kafka_producer_name -n tidb-cluster
+if [[ $kafka_producer_name != "" ]]
+then
+    kubectl delete deploy $kafka_producer_name -n tidb-cluster
+fi
 
 
